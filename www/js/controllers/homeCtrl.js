492788e749e93681e384314bd1b60ca97f2007d7
hafizbilal112 <hafizbilal112@gmail.com>
2015-08-06 15:03:47 +0500

    Worked on add transactions screen

--- www/js/controllers/homeCtrl.js
+++ www/js/controllers/homeCtrl.js
@@ -1,53 +1,87 @@
 angular.module('app')
-  .controller('homeCtrl', function ($app, $scope, $ionicPopover, $ionicModal) {
-    var home = this;
-    var categories = [
-      {id: 1, val: "Gadgets", color:"yellow"},
-      {id: 2, val: "Food", color: "indigo"},
-      {id: 3, val: "Clothes", color:"green"},
-      {id: 5, val: "Sales on Creative Market", color:"pink"},
-      {id: 5, val: "Jewelry", color:"purple"},
-      {id: 5, val: "Activities", color:"orange"},
-      {id: 5, val: "Sports", color:"red"}
-    ];
-    $scope.transactions = [
-      {category: "Gadgets", price: "1300", status:"loss"},
-      {category: "Clothes", price: "1300", status:"loss"},
-      {category: "Food", price: "1300", status:"profit"},
-      {category: "Sales", price: "1300", status:"loss"},
-      {category: "Gadgets", price: "1300", status:"loss"},
-      {category: "Clothes", price: "1300", status:"loss"},
-      {category: "Food", price: "1300", status:"loss"},
-      {category: "Sales", price: "1300", status:"loss"},
-      {category: "Gadgets", price: "1300", status:"loss"},
-      {category: "Gadgets", price: "1300", status:"loss"},
-      {category: "Clothes", price: "1300", status:"loss"},
-      {category: "Food", price: "1300", status:"loss"},
-      {category: "Sales", price: "1300", status:"loss"},
-      {category: "Clothes", price: "1300", status:"loss"},
-      {category: "Food", price: "1300", status:"loss"},
-      {category: "Sales", price: "1300", status:"loss"}
-    ];
-    $scope.options=['All Transactions','MCB ****1234','HBL ****1234'];
-    $scope.option=$scope.options[0];
-    $scope.selectOption=function (o) {
-      $scope.option=o;
-      $scope.popover.hide();
-    }
-    $scope.isSelectedOption=function (o) {
-      return $scope.option==o;
-    }
-    var template = '<ion-popover-view><ion-content><ion-item ng-click="selectOption(o)" ng-repeat="o in options" ng-class="{\'active\':isSelectedOption(o)}">{{o}}</ion-item></ion-content></ion-popover-view>';
-    $scope.popover = $ionicPopover.fromTemplate(template, {
-     scope: $scope
+    .controller('homeCtrl', function ($app, $scope, $ionicPopover, $ionicModal, $ionicPopup, $categories, $transactions) {
+        var home = this;
+        $scope.categories = $categories.get();
+        $scope.transactions = [
+            {category: "Gadgets", price: "1300", status: "loss"},
+            {category: "Clothes", price: "1300", status: "loss"},
+            {category: "Food", price: "1300", status: "profit"},
+            {category: "Sales", price: "1300", status: "loss"},
+            {category: "Gadgets", price: "1300", status: "loss"},
+            {category: "Clothes", price: "1300", status: "loss"},
+            {category: "Food", price: "1300", status: "loss"},
+            {category: "Sales", price: "1300", status: "loss"},
+            {category: "Gadgets", price: "1300", status: "loss"},
+            {category: "Gadgets", price: "1300", status: "loss"},
+            {category: "Clothes", price: "1300", status: "loss"},
+            {category: "Food", price: "1300", status: "loss"},
+            {category: "Sales", price: "1300", status: "loss"},
+            {category: "Clothes", price: "1300", status: "loss"},
+            {category: "Food", price: "1300", status: "loss"},
+            {category: "Sales", price: "1300", status: "loss"}
+        ];
+        $scope.newTran={};
+        $scope.newTran.cat="";
+        $scope.newTran.date=new Date();
+        $scope.selectCat= function (cat) {
+            $scope.newTran.cat=cat;
+        };
+        $scope.IsCat= function (cat) {
+            return $scope.newTran.cat==cat;
+        };
+        $scope.getCat= function (id) {
+            if(id)
+                return $categories.get(id).val
+        };
+        $scope.options = ['All Transactions', 'MCB ****1234', 'HBL ****1234'];
+        $scope.option = $scope.options[0];
+        $scope.selectOption = function (o) {
+            $scope.option = o;
+            $scope.popover.hide();
+        };
+        $scope.isSelectedOption = function (o) {
+            return $scope.option == o;
+        };
+        var template = '<ion-popover-view><ion-content><ion-item ng-click="selectOption(o)" ng-repeat="o in options" ng-class="{\'active\':isSelectedOption(o)}">{{o}}</ion-item></ion-content></ion-popover-view>';
+        $scope.popover = $ionicPopover.fromTemplate(template, {
+            scope: $scope
+        });
+
+        $scope.addTransaction = function () {
+            $ionicModal.fromTemplateUrl('templates/add-transaction.html', {
+                scope: $scope,
+                animation: 'slide-in-up'
+            }).then(function (modal) {
+                $scope.modal = modal;
+                $scope.modal.show();
+            });
+        };
+        $scope.showCat = function () {
+            $ionicPopup.show({
+                templateUrl: 'templates/categories.html',
+                title: 'Categories',
+                scope: $scope,
+                buttons: [
+                    {text: 'Cancel'},
+                    {
+                        text: '<b>Save</b>',
+                        type: 'button-positive',
+                        onTap: function (e) {
+                            if (!$scope.newTran.cat) {
+                                e.preventDefault();
+                            } else {
+                                return $scope.newTran.cat;
+                            }
+                        }
+                    }
+                ]
+            });
+        };
+
+        $scope.addTran= function () {
+            if($scope.newTran.price && $scope.newTran.cat && $scope.newTran.date){
+                $transactions.set($scope.newTran);
+                $scope.modal.remove();
+            }
+        }
     });
-    $scope.addTransaction=function () {
-      $ionicModal.fromTemplateUrl('templates/add-transaction.html', {
-        scope: $scope,
-        animation: 'slide-in-up'
-      }).then(function(modal) {
-        $scope.modal = modal;
-        $scope.modal.show();
-      });
-    }
-  })
--- www/js/services/categories.js
+++ www/js/services/categories.js
@@ -1,24 +1,35 @@
 angular.module('app')
-    .factory('$categories', function () {
-        var colors = [
-            {"id": "1", "val": "Gadgets", color: "purple"},
-            {"id": "2", "val": "Food", color: "yellow"}
+    .factory('$categories', function ($colors) {
+        var categories = [
+            {id: 1, val: "Gadgets", color: "yellow"},
+            {id: 2, val: "Food", color: "indigo"},
+            {id: 3, val: "Clothes", color: "green"},
+            {id: 4, val: "Sales on Creative Market", color: "pink"},
+            {id: 5, val: "Jewelry", color: "purple"},
+            {id: 6, val: "Activities", color: "orange"},
+            {id: 7, val: "Sports", color: "red"},
+            {id: 8, val: "Health", color: "green"},
+            {id: 9, val: "Books", color: "amber"}
         ];
         return {
             get: function (id) {
                 if (id) {
-                    var c= colors.filter(function (v) {
+                    var c= categories.filter(function (v) {
                         return v.id == id
                     });
                     if(c.length!=0){
-                        return c[0].val
+                        c.color=$colors.get(c.color);
+                        return c[0]
                     }
                     else{
                         return null
                     }
                 }
                 else {
-                    return colors
+                    return categories.map(function (v) {
+                        v.color=$colors.get(v.color);
+                        return v
+                    })
                 }
             }
         }
